f53a8c869160f950e44054b94dfc6b23d7a84299
Miguel Rosales <miguelrosales2027@gmail.com>
2022-10-18 17:48:43 -0700

    finished crazy cat and turtle shape

--- _01_elif/_3_are_you_happy/are_you_happy.py
+++ _01_elif/_3_are_you_happy/are_you_happy.py
@@ -6,3 +6,11 @@ if __name__ == '__main__':
     #       Use pop-ups to recreate the chart using if and elif statements
 
     pass
+    user_answer = simpledialog.askstring(None, "Are you happy?")
+
+
+    if(user_answer== "Yes" , "yes"):
+        messagebox.showinfo(None, "Keep doing what you are doing!")
+    if(user_answer=="no", "No"):
+        messagebox.showinfo(None, "Do you want to be happy?")
+
