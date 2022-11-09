91edcc6238dec6215c8e98701416d3a40d0a8dc7
Miguel Rosales <miguelrosales2027@gmail.com>
2022-11-08 18:27:23 -0800

    finished walk of fame and started 3-99

--- _02_loop_variables/_2_turtle_walk_of_fame/walk_of_fame.py
+++ _02_loop_variables/_2_turtle_walk_of_fame/walk_of_fame.py
@@ -1,5 +1,11 @@
 import turtle
 
+def Star():
+    for i in range(5):
+        my_turtle.forward(15)
+        my_turtle.right(144)
+        my_turtle.forward(15)
+
 if __name__ == '__main__':
     my_turtle = turtle.Turtle()
     my_turtle.shape('turtle')
@@ -8,9 +14,24 @@ if __name__ == '__main__':
 
     # TODO 1) Set the X position of the turtle so that it starts on the left.
 
+
+    my_turtle.penup()
+    my_turtle.goto(-300,0)
+    my_turtle.pendown()
     # TODO 2) Make the turtle draw a star shape. Hint: angle=144.
 
-    # TODO 3) Set the length of each line in the star to 30
+
+    for i in range(11):
+
+        my_turtle.penup()
+        my_turtle.forward(50)
+        my_turtle.pendown()
+        Star()
+
+
+
+
+          # TODO 3) Set the length of each line in the star to 30
 
     # TODO: CHALLENGE
     #       Make the turtle draw a line of stars like the image in
